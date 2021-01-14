2a8b197d91957e8a8eb4ff6fa306f90946774afc
Gh-Adam <ghada.adam.2011@gmail.com>
2021-01-14 10:57:48 +0200

    Adding Mock Data and DisplayColumns

---
+++ src/componets/DisplayedColumns.js
@@ -0,0 +1,22 @@
+export const displayedColumns = [
+  {
+    Header: 'Id',
+    accessor: 'id', // prop name in json
+  },
+  {
+    Header: 'First Name',
+    accessor: 'first_name',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'last_name',
+  },
+  {
+    Header: 'Email',
+    accessor: 'email',
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone',
+  },
+];
